1998ecfd0437e9e207da7f5ccf4d0fa5340c3d94
Kosodrzewinatru <sienkiew@live.com>
2018-12-27 15:55:00 +0100

    corrected code in Calculate.cs

--- Polish_Notation_Calculator/Models/Calculate.cs
+++ Polish_Notation_Calculator/Models/Calculate.cs
@@ -16,29 +16,29 @@ namespace Polish_Notation_Calculator
             return Source.Split(' ');
        }
 
-        public string toInfix()
-        {
-            string[] start = PrepareToGo();
-            Stack<string> expression = new Stack<string>();
-            for (int i = 0; i < start.Length; i++)
-            {
-                if (int.TryParse(start[i], out int result) == true)
-                    expression.Push(start[i]);
-                else
-                {
-                    string second = expression.Pop();
-                    string first = expression.Pop();
-                    string combinedElement = "(" + first + start[i] + second + ")";
-                    expression.Push(combinedElement);
-                }
-            }
-            return expression.Pop();
-        }
+       public string toInfix()
+       {
+           string[] start = PrepareToGo();
+           Stack<string> expression = new Stack<string>();
+           for (int i = 0; i < start.Length; i++)
+           {
+               if (int.TryParse(start[i], out int result) == true)
+                   expression.Push(start[i]);
+               else
+               {
+                   string second = expression.Pop();
+                   string first = expression.Pop();
+                   string combinedElement = "(" + first + start[i] + second + ")";
+                   expression.Push(combinedElement);
+               }
+           }
+           return expression.Pop();
+       }
 
-        public double Calc()
-        {
-            DataTable dt = new DataTable();
-            return Convert.ToDouble(dt.Compute(toInfix(), ""));
-        }
+       public double Calc()
+       {
+           DataTable dt = new DataTable();
+           return Convert.ToDouble(dt.Compute(toInfix(), ""));
+       }
     }
 }
