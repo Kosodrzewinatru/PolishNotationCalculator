6402b2e9dbc7cc07ed0ab3e3e464e67b0ce2a3bc
Kosodrzewinatru <sienkiew@live.com>
2019-02-21 19:31:56 +0100

    bug fixed (program crashed after typing more than one letter

--- Polish_Notation_Calculator/Models/Check.cs
+++ Polish_Notation_Calculator/Models/Check.cs
@@ -10,34 +10,55 @@ namespace Polish_Notation_Calculator.Controllers
         public string Source { get; set; }
         public string SourceTrue { get; set; }
 
+        //checking if typed content doesn't include letters
         public bool ifLetter()
         {
             string[] start = Source.Split(' ');
             string[] alphabet = { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
+            bool checker = true;
+
             for (int i = 0; i < start.Length; i++)
             {
-                for (int j = 0; j < alphabet.Length; j++)
+                for (int k = 0; k < start[i].Length; k++)
                 {
-                    if (start[i] == alphabet[j])
-                        return false;
+                    string element = start[i];
+                    for (int j = 0; j < alphabet.Length; j++)
+                    {
+                        if (element[k] == Convert.ToChar(alphabet[j]))
+                        {
+                            checker = false;
+                            break;
+                        }
+                    }
                 }
             }
-            return true;
+
+            return (checker == true) ? true : false;
         }
 
         public bool ifLetterTrue()
         {
             string[] start = SourceTrue.Split(' ');
             string[] alphabet = { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
+            bool checker = true;
+
             for (int i = 0; i < start.Length; i++)
             {
-                for (int j = 0; j < alphabet.Length; j++)
+                for (int k = 0; k < start[i].Length; k++)
                 {
-                    if (start[i] == alphabet[j])
-                        return false;
+                    string element = start[i];
+                    for (int j = 0; j < alphabet.Length; j++)
+                    {
+                        if (element[k] == Convert.ToChar(alphabet[j]))
+                        {
+                            checker = false;
+                            break;
+                        }
+                    }
                 }
             }
-            return true;
+
+            return (checker == true) ? true : false;
         }
     }
 }
